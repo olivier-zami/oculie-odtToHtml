a054c982a69db50e5a3b29cb109a93c95f5aff53
olivier <olivier@lain-1.0>
2021-04-09 22:23:02 +0200

    refactoring : separation odt parser class from ogt/html conversion function

--- ophir.php
+++ ophir.php
@@ -3,8 +3,8 @@
  * @file
  * ODT to HTML conversion functions
  *
- * Two functions are defined here : the first extracts the contents as XML from
- * the ODT file. The second parses the XML to produce HTML.
+ * Two functions are defined here : the first extracts the contents as XmlNode from
+ * the ODT file. The second parses the XmlNode to produce HTML.
  */
 
 /* Configuration.
@@ -60,7 +60,7 @@ function ophir_error($error){
 }
 
 /*
- * Function that parses the XML and outputs HTML. If $xml is not provided,
+ * Function that parses the XmlNode and outputs HTML. If $xml is not provided,
  * extract content.xml from $odt_file
  */
 function odt2html($odt_file, $xml_string=NULL) {
@@ -134,7 +134,7 @@ function odt2html($odt_file, $xml_string=NULL) {
                   $html .= "</" . $HTML_tag . ">";
               }
             }
-          } while ($xml->name !== $element["name"] && $element); //Close every opened tags. This should also handle malformed XML files
+          } while ($xml->name !== $element["name"] && $element); //Close every opened tags. This should also handle malformed XmlNode files
           continue;
     }
     elseif (in_array($xml->nodeType,
